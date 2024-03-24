e7154a3a0be7f5dd56fb18a3719fc912e59f689a
come_ill_foo <rayveleaveme34@inbox.ru>
2024-03-24 20:17:39 +0000

    feat(speeds): logs

--- rtts/rtt.sh
+++ rtts/rtt.sh
@@ -8,15 +8,15 @@ total=0
 min=1
 max=0
 while read num; do
-	total=$(echo "scale=10; ${total} + ${num}" | bc)
-	if [ 1 -eq "$(echo "scale=10; ${num} < ${min}" | bc)" ]; then  
+	total=$(echo "scale=16; ${total} + ${num}" | bc)
+	if [ 1 -eq "$(echo "scale=16; ${num} < ${min}" | bc)" ]; then  
 		min="${num}"
 	fi
-        if [ 1 -eq "$(echo "scale=10; ${num} > ${max}" | bc)" ]; then  
+        if [ 1 -eq "$(echo "scale=16; ${num} > ${max}" | bc)" ]; then  
 		max="${num}"
 	fi
 	count=$((count + 1))
 done < <(tshark -r "${INPUT}" -Tfields -e "tcp.analysis.ack_rtt" | sed '/^\s*$/d' -)
-avg=$(echo "scale=10; ${total} / ${count}" | bc)
+avg=$(echo "scale=16; ${total} / ${count}" | bc)
 printf "%zu\t%lf\t%lf\t%lf\n" "$count" "$avg" "$min" "$max"
 
